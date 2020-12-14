c9d1f25842e00a078886c836b34be8c2cd02400c
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2020-12-14 23:29:21 +0530

    Update exercise_1_Dictionary_definition_of_that_word.py

--- exercise_1_Dictionary_definition_of_that_word.py
+++ exercise_1_Dictionary_definition_of_that_word.py
@@ -1,4 +1,5 @@
 # Exercise-1 Answer
+# print the meaning of the word mentioned in the dictionary through user input
 print("Welcome to Dictionary")
 Dictionary={"mango": "fruit", "carrot": "vegetable", "burger": "fast food", "salad": "healthy food"}
 print("The required meaning is:")
