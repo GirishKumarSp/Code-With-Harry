06c9cbcfa17757601d98a936dec647bf56b7f509
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2020-12-14 23:14:45 +0530

    Add files via upload

---
+++ exercise_1_Dictionary_definition_of_that_word.py
@@ -0,0 +1,7 @@
+# Exercise-1 Answer
+print("Welcome to Dictionary")
+Dictionary={"mango": "fruit", "carrot": "vegetable", "burger": "fast food", "salad": "healthy food"}
+print("The required meaning is:")
+Search=input()
+print(Dictionary[Search])
+print("Dictionary Has Ended")
