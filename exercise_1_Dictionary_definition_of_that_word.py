3dc6969bea8bd460414e8a62b4c282e19b4bca83
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2020-12-16 11:29:01 +0530

    Delete exercise_1_Dictionary_definition_of_that_word.py

--- exercise_1_Dictionary_definition_of_that_word.py
+++
@@ -1,8 +0,0 @@
-# Exercise-1 Answer
-# print the meaning of the word mentioned in the dictionary through user input
-print("Welcome to Dictionary")
-Dictionary={"mango": "fruit", "carrot": "vegetable", "burger": "fast food", "salad": "healthy food"}
-print("The required meaning is:")
-Search=input()
-print(Dictionary[Search])
-print("Dictionary Has Ended")
