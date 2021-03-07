923e7d028edfea50829d3a632d147daab21b5d1a
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2021-03-07 21:05:38 +0530

    Delete 2_If_Else_or_elif.py

--- 2_If_Else_or_elif.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Your age please:"))
-if x > 18 and x < 101:
-    print("you can drive")
-elif x == 18:
-    print("we cannot tell if you can drive, you have to come and take a test for it")
-elif x < 18 and x > 4:
-    print("sorry you cannot drive")
-else:
-    print("Invalid Input!!!")
