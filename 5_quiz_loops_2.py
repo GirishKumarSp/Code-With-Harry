b0762a35da595051c88bb3691f9fb91da0a53bbf
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2021-03-07 21:06:27 +0530

    Delete 5_quiz_loops_2.py

--- 5_quiz_loops_2.py
+++
@@ -1,9 +0,0 @@
-while(True):
-    num = int(input("enter the number to evaluate: "))
-    if num < 100:
-        print("Try again")
-        continue
-    if num > 100:
-        print("yee you have entered more than 100")
-        break
-
